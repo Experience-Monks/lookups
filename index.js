ad9f00bb186ea6910bc1853e4337e2536cf09f82
David Sherret <dsherret@gmail.com>
2015-01-13 23:05:48 -0500

    Quicker lookups and sets for strings and numbers
    - Define methods on prototype (followed coding style from here: https://github.com/Jam3/innkeeper-socket.io/blob/master/index.js)
    - Strings and numbers are now in their own object for O(1) lookup. I used their own separate objects because the past functionality differentiated between the two. Arrays and functions are still O(n), but depending on the collection items it's potentially a smaller n now.

--- index.js
+++ index.js
@@ -1,65 +1,112 @@
-module.exports = function() {
+module.exports = lookup;
 
-  var data = Array.prototype.slice.call( arguments ),
-      getIdx = function( key ) {
-
-        var rVal = -1;
-
-        for( var i = 0, len = data.length; i < len; i += 2 ) {
-
-          if( data[ i ] === key ) {
-
-            rVal = i;
-            break;
-          }
-        }
-
-        return rVal;
-      };
+function lookup() {
 
   if( arguments.length % 2 != 0 ) {
 
     throw new Error( 'Incorrect key value amount. You should pass in arguments as key, value, key, value, ...' );
   }
 
-  return {
+  this._dataStr   = {}; // structure for strings
+  this._dataNum   = {}; // structure for numbers
+  this._dataOther = []; // structure for everything else
 
-    set: function( key, value ) {
+  for( var i = 0, len = arguments.length; i < len; i += 2 ) {
 
-      var i = getIdx( key );
+    this.set( arguments[ i ], arguments[ i + 1 ] );
+  }
+}
+ 
+lookup.prototype = {
+  set: function( key, value ) {
 
-      i = i == -1 ? data.length : i;
+    var keyType = typeof key;
 
-      data[ i ] = key;
-      data[ i + 1 ] = value;
+    if( keyType === "string" ) {
+
+      this._dataStr[ key ] = value;
+    }
+    else if( keyType === "number" ) {
+
+      this._dataNum[ key ] = value;
+    }
+    else {
+
+      var i = getIdx( this, key );
+
+      i = i == -1 ? this._dataOther.length : i;
+
+      this._dataOther[ i ]     = key;
+      this._dataOther[ i + 1 ] = value;
 
       return value;
-    },
+    }
+  },
 
-    remove: function( key ) {
+  remove: function( key ) {
 
-      var i = getIdx( key ),
-          rVal = undefined;
+    var keyType = typeof key;
+
+    if( keyType === "string" ) {
+
+      delete this._dataStr[ key ];
+    }
+    else if( keyType === "number" ) {
+
+      delete this._dataNum[ key ];
+    }
+    else {
+
+      var i = getIdx( this, key );
 
       if( i != -1 ) {
 
-        data.splice( i, 2 );
+        this._dataOther.splice( i, 2 );
       }
+    }
+  },
 
-      return rVal;
-    },
+  get: function( key ) {
 
-    get: function( key ) {
+    var keyType = typeof key;
 
-      var i = getIdx( key ),
-          rVal = undefined;
+    if( keyType === "string" ) {
+
+      return this._dataStr[ key ];
+    }
+    else if( keyType === "number" ) {
+
+      return this._dataNum[ key ];
+    }
+    else {
+
+      var i = getIdx( this, key )
 
       if( i != -1 ) {
 
-        rVal = data[ i + 1 ];
+        return this._dataOther[ i + 1 ];
       }
+      else {
 
-      return rVal;
-    } 
-  };
+        return undefined;
+      }
+    }
+  }
 };
+
+
+function getIdx( lookup, key ) {
+
+  var rVal = -1;
+
+  for( var i = 0, len = lookup._dataOther.length; i < len; i += 2 ) {
+
+    if( lookup._dataOther[ i ] === key ) {
+
+      rVal = i;
+      break;
+    }
+  }
+
+  return rVal;
+}
